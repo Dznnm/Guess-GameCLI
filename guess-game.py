a4ac75482c9cbdd90d39347fad4ff8d585be1127
Dznnm <danun.speed@gmail.com>
2026-06-20 19:37:52 +0700

    randomized number to guess

--- guess-game.py
+++ guess-game.py
@@ -1,13 +1,15 @@
+import random
+
 print("Welcome to Guess the Number Game!")
 difficulty = input("Enter your Difficulty (easy, medium, hard): ").lower()
 if difficulty == "easy":
-    number_to_guess = 5
+    number_to_guess = random.randint(1, 20)
     attempts = 10
 elif difficulty == "medium":
-    number_to_guess = 10
+    number_to_guess = random.randint(1, 50)
     attempts = 7
 elif difficulty == "hard":
-    number_to_guess = 15
+    number_to_guess = random.randint(1, 100)
     attempts = 5
 else:
     print("Invalid difficulty. Please choose easy, medium, or hard.")
